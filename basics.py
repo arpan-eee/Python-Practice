2a815346e3dcc43e4dc7bbf1a3ce82314125137b
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2024-07-01 11:31:36 +0600

    first commit

---
+++ basics.py
@@ -0,0 +1,23 @@
+print("Hello")
+print("Hello "*10)
+x = 1
+multi_line = """
+Hello,
+Multi Line
+"""
+print(multi_line)
+
+course = "Python Programming"
+print(len(course))
+print(course[1])
+print(course[1:3])
+print(course[:])
+print(course[:3])
+print(course[1:])
+
+course = "Python \"Programming"  # backslash to add special character
+print(course)
+course = "Python \nProgramming"
+print(course)
+
+first = "arpan"
